09d49945221ed7b5594f778019c7c7126faba15e
Super Stark <superstark02@gmail.com>
2020-05-09 12:39:32 +0530

    Updates

---
+++ precache-manifest.7deec193b44747218ad11857dbd39984.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4af04104ff5a7b3a11b94f5e3b52526",
+    "url": "/Pidgin/index.html"
+  },
+  {
+    "revision": "3a933fe8a42084b1ce98",
+    "url": "/Pidgin/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6351df23187d51b3497f",
+    "url": "/Pidgin/static/js/2.b9d6c9d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pidgin/static/js/2.b9d6c9d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a933fe8a42084b1ce98",
+    "url": "/Pidgin/static/js/main.38de39a2.chunk.js"
+  },
+  {
+    "revision": "f3f6a2295d56db3fc36f",
+    "url": "/Pidgin/static/js/runtime-main.8d516be7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Pidgin/static/media/logo.5d5d9eef.svg"
+  }
+]);
