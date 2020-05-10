b071a8490abc6bbabf007b366009cf87105f6769
Super Stark <superstark02@gmail.com>
2020-05-10 18:09:09 +0530

    Updates

--- precache-manifest.767ea29c977bdbe76607b82e8cafb057.js
+++ precache-manifest.767ea29c977bdbe76607b82e8cafb057.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "87f479509e988103948597efd282d63f",
+    "revision": "f3ad84fad35e423379cb22984f51cded",
     "url": "/Pidgin/index.html"
   },
   {
-    "revision": "0fb11e82a5897a893c29",
+    "revision": "b32ca6d0ce1cf2734bb4",
     "url": "/Pidgin/static/css/main.bb4e7405.chunk.css"
   },
   {
-    "revision": "5957c186813332da2329",
-    "url": "/Pidgin/static/js/2.4561c7ac.chunk.js"
+    "revision": "6a9ec961951b4c112b7c",
+    "url": "/Pidgin/static/js/2.a9b01d9e.chunk.js"
   },
   {
     "revision": "fdb2bbbbb647ba518ef54493c5cba538",
-    "url": "/Pidgin/static/js/2.4561c7ac.chunk.js.LICENSE.txt"
+    "url": "/Pidgin/static/js/2.a9b01d9e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0fb11e82a5897a893c29",
-    "url": "/Pidgin/static/js/main.06e1ec34.chunk.js"
+    "revision": "b32ca6d0ce1cf2734bb4",
+    "url": "/Pidgin/static/js/main.2ba00bed.chunk.js"
   },
   {
     "revision": "f3f6a2295d56db3fc36f",
