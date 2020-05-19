7d884209d75bb33f655d2d6bf44dbca5c88a706e
Super Stark <superstark02@gmail.com>
2020-05-19 10:56:54 +0530

    Updates

---
+++ precache-manifest.4a001b4187f43db753773022e7e97df8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fb64bb5522c111e7514ae983cde642",
+    "url": "/Pidgin/index.html"
+  },
+  {
+    "revision": "d02e317e80e423b0ea32",
+    "url": "/Pidgin/static/css/2.9e5c3921.chunk.css"
+  },
+  {
+    "revision": "9091d5c74ada62e32f31",
+    "url": "/Pidgin/static/css/main.7b4c2e6d.chunk.css"
+  },
+  {
+    "revision": "d02e317e80e423b0ea32",
+    "url": "/Pidgin/static/js/2.c965b8d7.chunk.js"
+  },
+  {
+    "revision": "827dafdf89c888d45ec5d95bfa3a4e09",
+    "url": "/Pidgin/static/js/2.c965b8d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9091d5c74ada62e32f31",
+    "url": "/Pidgin/static/js/main.2b950893.chunk.js"
+  },
+  {
+    "revision": "f3f6a2295d56db3fc36f",
+    "url": "/Pidgin/static/js/runtime-main.8d516be7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Pidgin/precache-manifest.88ca613289f9e152604e3fdc6f79bbf0.js"
+  "/Pidgin/precache-manifest.4a001b4187f43db753773022e7e97df8.js"
 );
 
 self.addEventListener('message', (event) => {
