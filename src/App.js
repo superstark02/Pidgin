f92f319597c39f3310ca1e0fa3a4416d5072876b
superstark02 <superstark02@gmail.com>
2020-08-14 12:22:42 +0530

    8/14/2020

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import ClassesDisplay from './Components/Class/ClassDisplay'
-import SearchView from './Filters/ClassFilter';
 import Course from './ClassComponents/course';
 import history from "./history";
-import OnGoingClasses from './HelpComponents/OnGoingClasses.js'
-import MyCart from './Cart/Cart';
-import EnrollmentForm from './HelpComponents/enrollmentForm';
-import Notifications from './HelpComponents/Notifications';
-import PaymentDone from './Cart/paymentDone';
-import PaymentNotDone from './Cart/paymentNotDone';
 import HomePage from './Pages/Home/HomePage';
 import SearchPage from './Pages/Search/SearchPage';
+import Cart from './Pages/Cart/Cart';
 
 class App extends React.Component{
 
@@ -20,12 +14,7 @@ class App extends React.Component{
     return (
       <Router history={history}>
         <Switch>
-          <Route exact path='/on_going' component={OnGoingClasses} ></Route>
-          <Route exact path='/success' component={PaymentDone} ></Route>
-          <Route exact path='/fail' component={PaymentNotDone} ></Route>
-          <Route exact path='/cart' component={MyCart} ></Route>
-          <Route exact path='/enrollmentForm' component={EnrollmentForm} ></Route>
-          <Route exact path='/notifications' component={Notifications} ></Route>
+          <Route exact path='/cart' component={Cart} ></Route>
           <Route exact path='/class-display/:id/:course_id' component={Course} ></Route>
           <Route exact path='/class-display/:id' component={ClassesDisplay} ></Route>
           <Route exact path='/class' component={HomePage} ></Route>
